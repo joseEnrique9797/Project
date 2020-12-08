18b6a5d8164cbcfe17d558eb273520f505e14fa2
joseEnrique9797 <48044322+joseEnrique9797@users.noreply.github.com>
2020-12-07 23:20:30 -0600

    Add files via upload

---
+++ Core/Python/DrawApplication/load.py
@@ -0,0 +1,93 @@
+# -*- coding:utf-8 -*-
+
+"""
+    @author: Barrientos
+    @date: 7/12/2020
+    @version 1.0
+"""
+
+from tkinter import *
+from tkinter import ttk
+#import tkinter as tk
+from tkinter import messagebox 
+#from Draw import *
+from  Core.Python.MySQLEngine import * 
+
+"""
+    loginGUI: Objeto para crear una ventana de Login
+"""
+class loginGUI:
+
+    """
+        Constructor para el objeto loginGUI
+    """
+    def __init__(self,user_id):
+        self.app = Tk()
+
+        #Título de la ventana
+        self.app.title('Load')
+
+        #Tamaño de la ventana
+        self.appWidth = 450
+        self.appHeight = 250
+        self.centerWindow()
+        #Titulo
+        self.label = Label(self.app, text='Seleccione la imagen que desea cargar',font = 'Helvetica 16 bold')
+        self.label.pack(pady=20)
+
+        #Contenido
+        self.combo = ttk.Combobox(self.app,width=17)
+       
+        self.combo.place(x=144,y=80)
+        options = []
+        
+        #Botón de Load , command=self.login
+        self.login = Button(self.app, command=self.load, text='Load',pady=5, padx=30)
+        self.login.place(x=100, y=170, width=100)
+
+        #Botón de Cancel , command=self.login
+        self.login = Button(self.app, command=self.close_window ,text='Cancel',pady=5, padx=30)
+        self.login.place(x=250, y=170, width=100)
+
+
+        SQLEngine = MySQLEngine()
+        SQLEngine.start()
+        #Consulta SQL para buscar al usuario 
+        _id = user_id
+        result = SQLEngine.select("SELECT Draw.var_name as 'nombre' FROM Library JOIN Draw ON Library.int_id_draw = Draw.id WHERE int_id_user = %s;" % _id)
+        print('------------------------------------',result)
+        
+        for rec in result:
+            options.append(rec)
+        self.combo['values']=options
+        
+        SQLEngine.close()
+    
+    def centerWindow(self):
+
+        xCoordinate = int(self.app.winfo_screenwidth()/2 - self.appWidth/2)
+        yCoordinate = int(self.app.winfo_screenheight()/2 - self.appHeight/2)
+
+        self.app.geometry('%sx%s+%s+%s' % (self.appWidth,self.appHeight,xCoordinate,yCoordinate))
+
+    
+    def close_window(self):
+        self.app.destroy() 
+    
+    def load(self):
+        imageName = self.combo.get()
+        if not imageName:
+            messagebox.showerror('Error', 'Seleccione una imagen de la lista, si la lista esta vacia oprima Cancel')
+        else:
+            SQLEngine = MySQLEngine()
+            SQLEngine.start()
+            result = SQLEngine.select("SELECT * FROM Draw WHERE var_name = '%s';" % imageName)
+
+        print('True-------------------',result)
+        
+        
+
+    
+    def run(self):
+        self.app.mainloop()
+  
